58e3316dc3cf8914887b70711496671fdfa6bf10
ornato_t <tommy.ornato@gmail.com>
2023-08-19 22:38:04 +0200

    Adding Tailwind and DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome</h1>
+<h1 class="text-red-500">Welcome</h1>
 <p>Check the statistics for <a href="/check/lemmy.basedcount.com">lemmy.basedcount.com</a> at this link.</p>
+<div class="btn">This doesn't do anything</div>
