483772f13580cb337f0eaae91f378604ed3e9353
ornato_t <tommy.ornato@gmail.com>
2023-08-20 00:36:39 +0200

    Styled homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,29 @@
     import '../app.css';
 </script>
 
+<svelte:head>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</svelte:head>
+
+<div class="navbar bg-base-100">
+    <div class="navbar-start">
+        <a class="btn btn-outline btn-secondary normal-case text-sm px-2 md:px-4" href="https://lemmy.basedcount.com">
+            <span class="hidden md:block">
+                Powered by Based Count
+            </span>
+            <img class="h-8 w-8 md:ml-1.5 md:mt-1.5" src="https://lemmy.basedcount.com/pictrs/image/7128ea29-f4bf-4602-b1b7-5517a09ae3f4.png" alt="BasedCount pill">
+        </a>
+
+    </div>
+
+    <div class="navbar-center">
+      <a class="btn btn-ghost normal-case text-xl" href="/">Lemmy Defed-Investigator</a>
+    </div>
+
+    <div class="navbar-end">
+        <a href="https://github.com/ornato-t/lemmy-defed-investigator" class="btn btn-ghost hidden md:flex">
+            <i class='bx bxl-github text-4xl'></i>
+        </a>
+    </div>     
+</div>
 <slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,26 @@
-<h1 class="text-red-500">Welcome</h1>
-<p>Check the statistics for <a href="/check/lemmy.basedcount.com">lemmy.basedcount.com</a> at this link.</p>
-<div class="btn">This doesn't do anything</div>
+<div class="hero min-h-[calc(100vh-4rem)] bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-lg">
+        <h1 class="text-5xl font-bold">Investigate defederation</h1>
+        <div class="py-6">
+            <p>
+                Find out which Lemmy instances have blocked / defederated yours.
+            </p>
+            <p>
+                Enter the domain (URL) of your Lemmy instance and press enter.
+            </p>
+        </div>
+
+        <input type="text" placeholder="lemmy.example.com" class="input input-bordered input-primary w-full max-w-xs" />
+
+        <div class="py-4 text-sm">
+            <p>
+                <span class="font-bold">How does this work?</span> The Investigator will query every Lemmy instance through your browser. The search will take approximately 1-2 minutes. 
+            </p>
+            <p>
+                The full list of instances is available <a href="https://github.com/maltfield/awesome-lemmy-instances" class="link">here</a>.
+            </p>
+        </div>
+      </div>
+    </div>
+  </div>
