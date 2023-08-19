d7c8255838516d3e64c7cf08b77d2fb085c53fd3
ornato_t <tommy.ornato@gmail.com>
2023-08-19 19:35:45 +0200

    Moving requests to client

--- src/routes/check/[name]/+page.server.ts
+++ src/routes/check/[name]/+page.server.ts
@@ -1,5 +1,4 @@
-import { fetchInstances, type Instance as InstanceInit } from "$lib/instanceList";
-import type { GetFederatedInstancesResponse } from 'lemmy-js-client';
+import { fetchInstances } from "$lib/instanceList";
 
 export async function load({ params, fetch }) {
     const instances = await fetchInstances();
@@ -12,56 +11,7 @@ export async function load({ params, fetch }) {
 
     return {
         name: name,
-        instances: instances.map(el => checkFederation(name, el, fetch)),
-        total: instances.length,
+        instances,
         warning: warning
     };
-}
-
-//Check whether an instance is federated with the queried instance
-async function checkFederation(name: string, instance: InstanceInit, fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>): Promise<Instance> {
-    let linked: boolean | undefined;
-    let blocked: boolean | undefined;
-    let error: boolean;
-
-    try {
-        const federation = await fetch('/proxy', { method: 'POST', body: JSON.stringify({ url: instance.url }) }) as GetFederatedInstancesResponse;
-
-        const blockedList = federation.federated_instances?.blocked;
-        const linkedList = federation.federated_instances?.linked;
-
-        if (blockedList === undefined) throw new Error();
-        if (linkedList === undefined) throw new Error();
-
-        if (blockedList.map(instance => instance.domain).includes(name)) {
-            blocked = true;
-        } else {
-            blocked = false;
-        }
-        if (linkedList.map(instance => instance.domain).includes(name)) {
-            linked = true;
-        } else {
-            linked = false;
-        }
-
-        error = false;
-    } catch (e) {
-        error = true;
-    }
-
-    return {
-        name: instance.name,
-        url: instance.url,
-        linked,
-        blocked,
-        error,
-    };
-}
-
-interface Instance {
-    name: string;                   //Instance name
-    url: string;                    //Instance domain
-    linked: boolean | undefined;    //Instance linked with queried instance
-    blocked: boolean | undefined;   //Instance has blocked/defederated the queried instance
-    error: boolean                  //Error during lookup
 }
---
+++ src/routes/check/[name]/+page.ts
@@ -0,0 +1,59 @@
+import type { Instance as InstanceInit } from "$lib/instanceList";
+import type { GetFederatedInstancesResponse } from 'lemmy-js-client';
+
+export async function load({ fetch, data }) {
+    return {
+        name: data.name,
+        instances: data.instances.map(el => checkFederation(data.name, el, fetch)),
+        total: data.instances.length,
+        warning: data.warning
+    };
+}
+
+//Check whether an instance is federated with the queried instance
+async function checkFederation(name: string, instance: InstanceInit, fetch: (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>): Promise<Instance> {
+    let linked: boolean | undefined;
+    let blocked: boolean | undefined;
+    let error: boolean;
+
+    try {
+        const federation = await fetch('/proxy', { method: 'POST', body: JSON.stringify({ url: instance.url }) }) as GetFederatedInstancesResponse;
+
+        const blockedList = federation.federated_instances?.blocked;
+        const linkedList = federation.federated_instances?.linked;
+
+        if (blockedList === undefined) throw new Error();
+        if (linkedList === undefined) throw new Error();
+
+        if (blockedList.map(instance => instance.domain).includes(name)) {
+            blocked = true;
+        } else {
+            blocked = false;
+        }
+        if (linkedList.map(instance => instance.domain).includes(name)) {
+            linked = true;
+        } else {
+            linked = false;
+        }
+
+        error = false;
+    } catch (e) {
+        error = true;
+    }
+
+    return {
+        name: instance.name,
+        url: instance.url,
+        linked,
+        blocked,
+        error,
+    };
+}
+
+interface Instance {
+    name: string;                   //Instance name
+    url: string;                    //Instance domain
+    linked: boolean | undefined;    //Instance linked with queried instance
+    blocked: boolean | undefined;   //Instance has blocked/defederated the queried instance
+    error: boolean                  //Error during lookup
+}
