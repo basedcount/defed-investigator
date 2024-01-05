d1c7bdccb80fe314974953b46e9eefd86ec385ea
Tommaso Ornato <tommy.ornato@gmail.com>
2024-01-05 15:38:29 +0100

    Update pill image, slightly reword text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,9 @@
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
     <!-- Primary Meta Tags -->
-    <title>Defederation Investigator for Lemmy</title>
-    <meta name="title" content="Defederation Investigator for Lemmy">
-    <meta name="description" content="Discover who has defederated your Lemmy instance with the Defederation Investigator.">
+    <title>Defederation Investigator</title>
+    <meta name="title" content="Defederation Investigator">
+    <meta name="description" content="Discover who has defederated your Fediverse instance with the Defederation Investigator.">
 
     <meta name="keywords" content="lemmy,defederation,defed,investigator,investigation,federation,fediverse,mastodon,kbin,peertube,basedcount">
     <meta name="robots" content="index, follow">
@@ -19,15 +19,15 @@
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://defed.xyz/" />
-    <meta property="og:title" content="Defederation Investigator for Lemmy" />
-    <meta property="og:description" content="Discover who has defederated your Lemmy instance with the Defederation Investigator." />
+    <meta property="og:title" content="Defederation Investigator" />
+    <meta property="og:description" content="Discover who has defederated your Fediverse instance with the Defederation Investigator." />
     <meta property="og:image" content="https://defed.xyz/favicon.png" />
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://defed.xyz" />
-    <meta property="twitter:title" content="Defederation Investigator for Lemmy" />
-    <meta property="twitter:description" content="Discover who has defederated your Lemmy instance with the Defederation Investigator." />
+    <meta property="twitter:title" content="Defederation Investigator" />
+    <meta property="twitter:description" content="Discover who has defederated your Fediverse instance with the Defederation Investigator." />
     <meta property="twitter:image" content="https://defed.xyz/favicon.png" />
 
     
@@ -48,13 +48,13 @@
             <span class="hidden md:block">
                 Powered by Based Count
             </span>
-            <img class="h-8 w-8 md:ml-1.5 md:mt-1.5" src="/favicon.png" alt="BasedCount pill">
+            <img class="h-8 w-8 md:ml-1.5 md:mt-1.5" src="/pill.png" alt="BasedCount pill">
         </a>
 
     </div>
 
     <div class="navbar-center">
-      <a class="btn btn-ghost normal-case text-xl cursor-pointer" href="/">Lemmy Defed-Investigator</a>
+      <a class="btn btn-ghost normal-case text-xl cursor-pointer" href="/">Defed Investigator</a>
     </div>
 
     <div class="navbar-end">
@@ -63,4 +63,5 @@
         </a>
     </div>     
 </div>
+
 <slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,42 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
+    import { goto } from "$app/navigation";
 
     let instance: string;
 
     function query() {
-        if(instance.length > 0) goto(`/check/${removeProtocol(instance).toLowerCase()}`);
+        if (instance.length > 0) goto(`/check/${removeProtocol(instance).toLowerCase()}`);
     }
 
     //If the user submitted URL includes the protocol, cut it out
     function removeProtocol(url: string) {
-        if (url.startsWith("http://"))
-            return url.slice(7);
-        else if (url.startsWith("https://"))
-            return url.slice(8);
+        if (url.startsWith("http://")) return url.slice(7);
+        else if (url.startsWith("https://")) return url.slice(8);
 
         return url;
     }
-
 </script>
 
 <div class="hero min-h-[calc(100vh-4rem)] bg-base-200">
     <div class="hero-content text-center">
-      <div class="max-w-lg">
-        <h1 class="text-5xl font-bold">Investigate defederation</h1>
-        <div class="py-6">
-            <p>
-                Find out which Lemmy instances have blocked / defederated yours.
-            </p>
-            <p>
-                Enter the domain (URL) of your Lemmy instance and press enter.
-            </p>
-        </div>
+        <div class="max-w-lg">
+            <h1 class="text-5xl font-bold">Investigate defederation</h1>
+            <div class="py-6">
+                <p>Find out which Fediverse instances have blocked / defederated yours.</p>
+                <p>Enter the domain (URL) of your instance and press enter.</p>
+            </div>
 
-        <form on:submit|preventDefault={query} class="w-full flex flex-row place-content-center gap-x-2">
-            <input type="text" placeholder="lemmy.example.com" class="input input-bordered input-primary w-full max-w-xs" bind:value={instance}/>
-            <button class="btn btn-outline btn-primary" aria-label="Search">
-                <i class='bx bx-search-alt text-xl'></i>
-            </button>
-        </form>
+            <form on:submit|preventDefault={query} class="w-full flex flex-row place-content-center gap-x-2">
+                <input type="text" placeholder="lemmy.example.com" class="input input-bordered input-primary w-full max-w-xs" bind:value={instance} />
+                <button class="btn btn-outline btn-primary" aria-label="Search">
+                    <i class="bx bx-search-alt text-xl"></i>
+                </button>
+            </form>
 
-        <div class="py-6 text-sm text-acc">
-            <p>
-                <span class="font-bold text-accent-focus">How does this work?</span> The Investigator will query every Lemmy instance through your browser.
-         
-                The full list of instances is available in <a href="https://github.com/maltfield/awesome-lemmy-instances" class="link">this repository</a>.
-            </p>
+            <div class="py-6 text-sm text-acc">
+                <p>
+                    <span class="font-bold text-accent-focus">How does this work?</span> The Investigator will query every Lemmy instance through your browser. The full list of instances is available in <a href="https://github.com/maltfield/awesome-lemmy-instances" class="link">this repository</a>.
+                </p>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
